fb389650d267cecb9f5f76ca4022663658430828
Marcus Almeida <mpereirassa@gmail.com>
2025-12-06 20:30:54 -0300

    feat: move configuração do Tailwind para arquivo separado assets/js/config.js

---
+++ assets/js/config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                cream: '#0B0B0F',
+                brand: {
+                    purple: '#7C3AED',
+                    light: '#A78BFA',
+                    dark: '#5B21B6',
+                    soft: '#1C1C26',
+                    surface: '#13131A',
+                }
+            },
+            fontFamily: {
+                sans: ['Outfit', 'sans-serif'],
+                display: ['Fredoka', 'sans-serif'],
+                hand: ['Dancing Script', 'cursive'],
+            }
+        }
+    }
+}
